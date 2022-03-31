e73edac4fa0f212eb700319f95e12cae947da970
CarlosHLeite <carlosleiteh@gmail.com>
2022-03-30 23:45:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />} >
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Posts />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+        <h2>Carlos Blog</h2>   
+    </nav>
+  )
+}
+
+export default Navbar
